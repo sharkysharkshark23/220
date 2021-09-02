d89d7676acaaa6563f160d534aa400e76df19575
thomas <scolatj@g,cofc.edu>
2021-09-02 19:35:25 -0400

    assignment 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,11 @@
+"""
+Thomas Scola
+hello.py
+problem: This program print 'hello, world!' on the screen
+Certificate of Authenticity: This work is completely my work
+
+"""
+def main():
+    print("hello, world!")
+
+main()
