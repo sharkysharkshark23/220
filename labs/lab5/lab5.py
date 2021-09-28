19bd64295872636e206bd60b68a9ac53320470ba
thomas <scolatj@g,cofc.edu>
2021-09-28 16:48:23 -0400

    lab5

--- labs/lab5/lab5.py
+++ labs/lab5/lab5.py
@@ -1,9 +1,11 @@
+import math
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Thomas Scola
+lab5.py
 """
 
 from graphics import *
+
 
 
 def target():
@@ -21,7 +23,36 @@ def target():
 def triangle():
     win_width = 500
     win_height = 500
-    win = GraphWin("Draw a Triangle", win_width, win_height)
+    win = GraphWin("Draw a Triangle", 500, 500)
+
+    p1 = win.getMouse()
+    p1.draw(win)
+    p2 = win.getMouse()
+    p2.draw(win)
+    p3 = win.getMouse()
+    p3.draw(win)
+
+    triangle = Polygon(p1, p2, p3)
+    triangle.draw(win)
+
+    p1x = p1.getX()
+    p1y = p1.getY()
+    p2x = p2.getX()
+    p2y = p2.getY()
+    p3x = p3.getX()
+    p3y = p3.getY()
+
+    #calculations
+    length1 = math.sqrt(((p2x - p1x)**2) + ((p2y - p1y)**2))
+    length2 = math.sqrt(((p2x - p3x)**2) + ((p2y - p3y)**2))
+    length3 = math.sqrt(((p3x - p1x)**2) + ((p3y - p1y)**2))
+
+    perimeter = length1 + length2 + length3
+    s = perimeter / (2)
+    area = math.sqrt(s*(s - length1)*(s - length2)*(s - length3))
+
+    print(perimeter)
+    print(area)
 
     # Add code here to accept the mouse clicks, draw the triangle.
     # and display its area in the graphics window.
@@ -29,8 +60,7 @@ def triangle():
     # Wait for another click to exit
     win.getMouse()
     win.close()
-
-
+triangle()
 def color_shape():
     '''Create code to allow a user to color a shape by entering rgb amounts'''
 
@@ -75,11 +105,45 @@ def color_shape():
     win.getMouse()
     win.close()
 
+def process_string():
+    strng = input("enter your string")
+    print(strng[0])
+    print(strng[10])
+    print(strng[2:5])
+    print(strng[0]+strng[-1])
+    print(strng[0:3]*10)
+    for char in strng():
+        print(strng)
+    print(len(strng))
+
+process_string()
+
+def process_list():
+    pt = Point(5,10)
+    values = [5, "hi", 2.5, "there", pt, "7.2"]
+    x = values[1,3]
+    print(x)
+    x = values[0] + values[2]
+    print(x)
+    x = values[1] * 5
+    print(x)
+    x = values[2, 3, 4]
+    print(x)
+    x = values[2, 3, 0]
+    print(x)
+    x = values[2, 0, 5]
+    print(x)
+    x = values[0] + values[2] + values[5]
+    print(x)
+    print(len(x))
+process_list()
 
 def main():
     # target()
     # triangle()
     # color_shape()
+    #process_string()
+    #process_list()
     pass
 
 
