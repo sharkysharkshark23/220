b80662ad1f01da465cb876e5c9db7545208c3e50
thomas <scolatj@g,cofc.edu>
2021-10-12 16:23:19 -0400

    lab7

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -1,13 +1,29 @@
 """
-Name: <your name goes here – first and last>
-Partner: <your partner's name goes here – first and last>
-<ProgramName>.py
+Name: Thomas Scola
+Partner: I did this at home becuase I was studying for a midterm
+I certify this is my work and my work only
+lab7.py
 """
 
-
 def main():
+
+def cash_conversion():
+    x = input("Input cash amount")
+    print('{}'.format('x'))
+    [Output: x]
+    print('{}'.format(x))
+    print('{:.2}'.format(x))
+
+cash_conversion()
+
+def encode():
+
+
+
+
+
     # add function calls here
-    pass
+    #pass
 
 
 main()
